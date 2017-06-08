84d396d3428cd411cfec7963e13b2c70e392629f
Stephanie Clement <sclement@uvic.ca>
2017-06-07 21:15:11 -0700

    Added polarity feature

--- feature_generation.py
+++ feature_generation.py
@@ -3,15 +3,15 @@
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
-from FeatureData import FeatureData
+from FeatureData import FeatureData, tokenize_text
 
 
 class FeatureGenerator(object):
     """Class responsible for generating each feature used in the X matrix."""
 
     def __init__(self, clean_articles, clean_stances):
-        self.articles = clean_articles
-        self.stances = clean_stances
+        self.articles = clean_articles # dictionary {article ID: body}
+        self.stances = clean_stances # list of dictionaries
 
     def get_features(self):
         """Retrieves the full set of features as a matrix (the X matrix for training.)"""
@@ -72,13 +72,33 @@ class FeatureGenerator(object):
         #print "[DEBUG] features", features
         return features
 
+    def _polarity_feature(self):
+        _refuting_words = [ 'fake', 'fraud', 'hoax', 'false', 'deny', 'denies', 'not',
+        'despite', 'nope', 'nowhere', 'doubt', 'doubts', 'bogus', 'debunk', 'pranks',
+        'retract', 'nothing', 'never', 'none', 'budge']
+
+
+        def determine_polarity(text):
+            tokens = tokenize_text(text)
+            return sum([token in _refuting_words for token in tokens]) % 2
+
+        X = []
+        for dict in self.stances:
+            features = []
+            features.append(determine_polarity(dict['Headline']))
+            features.append(determine_polarity(self.articles.get(dict['Body ID'])))
+            X.append(features)
+
+        return np.array(X)
+
+
 if __name__ == '__main__':
     feature_data = FeatureData('data/train_bodies.csv', 'data/train_stances.csv')
     feature_generator = FeatureGenerator(feature_data.get_clean_articles(), feature_data.get_clean_stances())
 
     feature_generator._get_ngrams(3)
-    # feature_generator._get_refuting_words()
-
+    feature_generator._get_refuting_words()
+    X = feature_generator._polarity_feature()
 
 
 
